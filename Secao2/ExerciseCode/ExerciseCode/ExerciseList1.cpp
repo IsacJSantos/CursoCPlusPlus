41fef094d904a2e497eb150355fc0ecb988e31a8
IsacJSantos <isac.santos@electricmonkeys.games>
2025-06-05 10:29:01 -0300

    Section exercise completed

---
+++ Secao2/ExerciseCode/ExerciseCode/ExerciseList1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Vou" << std::endl
+		<< "aprender" << std::endl
+		<< "a" << std::endl
+		<< "programar" << std::endl
+		<< "na" << std::endl
+		<< "linguagem C++" << std::endl;
+	
+	system("color 4a");
+	system("PAUSE");
+
+	return 0;
+}
