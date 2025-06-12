15051e9879b0baf272074c579b3b237388667f2e
IsacJSantos <isac.santos@electricmonkeys.games>
2025-06-12 13:00:48 -0300

    consts

---
+++ Secao4/Consts/Consts/Constantes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	const int NUM_VIDAS = 10;
+	int totalDeVidas;
+	totalDeVidas = NUM_VIDAS - 1;
+
+	std::cout << "Total de vidas: " << totalDeVidas << std::endl;
+	std::cout << "Valor constante NUM_VIDAS: " << NUM_VIDAS << std::endl;
+	system("PAUSE");
+}
