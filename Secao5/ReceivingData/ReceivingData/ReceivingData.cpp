eef109b54bccc170b4300ccf66ec1eb64cb2429e
IsacJSantos <isac4322@gmail.com>
2025-06-14 12:28:43 -0300

    receiving data with cin

---
+++ Secao5/ReceivingData/ReceivingData/ReceivingData.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	int number01, number02;
+	std::cout << "Enter the first number: ";
+	std::cin >> number01;
+	std::cout << "number01: " << number01 << std::endl;
+
+	std::cout << "Enter the second number: ";
+	std::cin >> number02;
+	std::cout << "number02: " << number02 << std::endl;
+
+
+	system("Pause");
+}
+
