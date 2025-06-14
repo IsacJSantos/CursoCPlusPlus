5ad9f0e93f5d761b0fa4a2dd1bbbda0a11ccfe82
IsacJSantos <isac4322@gmail.com>
2025-06-14 12:45:22 -0300

    arithmetic operators

--- Secao5/ReceivingData/ReceivingData/ReceivingData.cpp
+++ Secao5/ReceivingData/ReceivingData/ReceivingData.cpp
@@ -3,6 +3,7 @@
 int main()
 {
 	int number01, number02;
+	int result;
 	std::cout << "Enter the first number: ";
 	std::cin >> number01;
 	std::cout << "number01: " << number01 << std::endl;
@@ -11,6 +12,22 @@ int main()
 	std::cin >> number02;
 	std::cout << "number02: " << number02 << std::endl;
 
+	std::cout << "-----------------------------" << std::endl;
+
+	result = number01 + number02;
+	std::cout << "Sum: " << result << std::endl;
+
+	result = number01 - number02;
+	std::cout << "Subtraction: " << result << std::endl;
+
+	result = number01 * number02;
+	std::cout << "Multiplication: " << result << std::endl;
+
+	result = number01 / number02;
+	std::cout << "Division: " << result << std::endl;
+
+	result = number01 % number02;
+	std::cout << "Modulus: " << result << std::endl;
 
 	system("Pause");
 }
