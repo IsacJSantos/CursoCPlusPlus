3c870fcc92273dcd758cf402824cffc986c44d8b
IsacJSantos <isac.santos@electricmonkeys.games>
2025-06-25 09:04:25 -0300

    void methods

--- Secao8/Methods/Methods/Methods.cpp
+++ Secao8/Methods/Methods/Methods.cpp
@@ -5,6 +5,11 @@ int Sum(int a, int b)
 	return a + b;
 }
 
+void Message()
+{
+	std::cout << "\nA reminder that life is never easy\n";
+}
+
 int main()
 {
 	int a, b;
@@ -13,5 +18,7 @@ int main()
 	std::cout << "\nEnter the second number: ";
 	std::cin >> b;
 	std::cout << "\nThe sum is: " << Sum(a, b) << std::endl;
+
+	Message();
 	system("Pause");
 }
