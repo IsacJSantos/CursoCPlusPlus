b37425b039f3221bd4d2d51217420a461a04fdd6
IsacJSantos <isac.santos@electricmonkeys.games>
2025-07-15 08:54:19 -0300

    More details about references

--- Secao10/References/References/References.cpp
+++ Secao10/References/References/References.cpp
@@ -3,6 +3,7 @@
 int main()
 {
 	int num{ 1024 };
+	int num2{ 64 };
 	// 'int& numRef' é um alias para o mesmo campo de momória que 'num'
 	// Não corresponde a um novo valor na memória, e sim a um novo identificador para
 	// um endereço existente
@@ -18,6 +19,16 @@ int main()
 	numRef = 10;
 	std::cout << "\n";
 	std::cout << "Valor em num: " << num << "\n";
+	std::cout << "Valor em &numRef: " << numRef << "\n";
+
+	// Diferentemente de ponteiros, ref pode ser atribuída somente uma única vez.
+	// Sendo assim 'numRef = num2' apenas recebe o valor contido em num2 no endereço de memória
+	//Referenciado em 'numRef'
+	numRef = num2; 
+	num2 = 0;
+	std::cout << "\n";
+	std::cout << "Valor em num: " << num << "\n";
+	std::cout << "Valor em num2: " << num2 << "\n";
 	std::cout << "Valor em &numRef: " << numRef << "\n";
 
 	system("Pause");
